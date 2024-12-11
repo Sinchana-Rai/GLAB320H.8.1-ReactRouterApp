5b1d7ae87ea4dc98b41b96181854cc3719e884e6
Sinchana-Rai <sinchana.srai@gmail.com>
2024-12-10 21:21:14 -0500

    react router application

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import "./App.css";
+
+// Import Route and our components
+import { Route, Routes } from "react-router-dom";
+import Currencies from "./pages/Currencies";
+import Main from "./pages/Main";
+import Price from "./pages/Price";
+import Nav from "./components/Nav";
+
+export default function App () {
+  return (
+    <div className="App">
+       <Nav />
+       <Routes>
+         <Route path="/" element={<Main />} />
+         <Route path="/currencies" element={<Currencies />} />
+         <Route path="/price" element={<Price />} />
+       </Routes>
+    </div>
+  );
+}
+
+
+
+//======================================
+
+// import "./App.css";
+
+// // Import Route and our components
+// import { Route, Routes } from "react-router-dom";
+// import Currencies  from "./pages/Currencies";
+// import Main from "./pages/Main";
+// import Price from "./pages/Price";
+// import Nav from "./components/Nav";
+
+// export default function App () {
+//   // We will use the Route component, path attribute, and element attribute to specify each route.
+//   return (
+//     <>
+//     <div className="App">
+
+//        <Nav />
+//       <Routes>
+//         <Route path="/" element={<Main/>}/>
+//         <Route path="/currencies" element={<Currencies/>}/>
+//         <Route path="/price" element={<Price/>}/>
+//       </Routes>
+//     </div>
+//     </>
+//   );
+// }
